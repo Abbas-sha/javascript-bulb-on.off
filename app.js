368ea4d6f44a02b1cbfc51313789174c6b8a0168
Abbas-sha <attaeabbasshah@gmail.com>
2023-08-15 16:59:04 -0400

    Done

---
+++ app.js
@@ -0,0 +1,13 @@
+// function foo(){
+//     var a = document.getElementById("para")
+//     a.innerHTML='lorem'
+// }
+
+function bulbon(){
+    document.getElementById ("abc").src = './img/bulb 1.jpg' 
+}
+
+function bulboff(){
+    document.getElementById ("abc").src = './img/bulb 2.jpg' 
+
+}
